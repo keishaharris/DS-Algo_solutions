11b629c3c1bf6082745fb0622ef9d7c9e7b74da9
keishaharris <keishadharriss@gmail.com>
2023-04-19 15:52:48 -0400

    solution for finding head/tail value, deletion,index Value/index of value, string of all value in list

--- linked_list.js
+++ linked_list.js
@@ -34,6 +34,68 @@ class LinkedList{
         }
         return total;
     }
+
+    getHeadValue(){
+        if(this.head === null) return null;
+        return this.head.value;
+    }
+
+    getTailValue(){
+        let temp = this.head;
+        while(temp.nextNode !== null){
+            temp = temp.nextNode;
+        }
+        return temp.value; 
+    }
+
+    findIndexValue(index){
+        let tmpIndex = 0;
+        let temp = this.head;
+        while(temp.nextNode !== null && tmpIndex !== index){
+            temp = temp.nextNode;
+            tmpIndex++;
+        }
+        return temp.value;
+    }
+
+    popElement(){
+        if(!this.head) return 'Empty List'
+        let curr = this.head;
+        let prev = null;
+        while(curr.nextNode !== null){
+            prev = curr;
+            curr = curr.nextNode;
+        }
+        prev ? prev.nextNode = null : this.head = null;
+    }
+
+    containsValue(value){
+        let temp = this.head
+        while(temp !== null){
+            if(temp.value == value) return true;
+            temp = temp.nextNode;
+        }
+        return false;
+    }
+
+    findIndex(value){
+        let index = 0;
+        let temp = this.head;
+        while(temp !== null) {
+            if(temp.value == value) return index;
+            index++;
+            temp = temp.nextNode;
+        }
+    }
+
+    listToString(){
+        let temp = this.head;
+        while(temp.nextNode !== null){
+            console.log(`(${temp.value}) -->`)
+            temp = temp.nextNode;
+        }
+        console.log(`${temp.nextNode}`);
+    }
 }
 
 
@@ -51,4 +113,10 @@ lnkList.append(3);
 lnkList.append(4);
 lnkList.prepend(1);
 
-console.log(lnkList.size()); //4
+console.log('Size', lnkList.size()); //4
+console.log('Get Head Value', lnkList.getHeadValue()); //1
+console.log('Get Tail Value', lnkList.getTailValue()); //4
+console.log('Find Index Value', lnkList.findIndexValue(2)) //3
+console.log('Contains Value', lnkList.containsValue(2)); //true
+console.log('Find Index of Value', lnkList.findIndex(1)); //0
+console.log('Conver to String', lnkList.listToString())// (1) --> (2) --> (3) --> (4) --> null
