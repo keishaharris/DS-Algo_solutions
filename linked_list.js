b67e7370258fd1e44d694c716d11ba1b06d6a8e7
keishaharris <keishadharriss@gmail.com>
2023-04-19 22:20:19 -0400

    solutions to inserting and deleting a given index

--- linked_list.js
+++ linked_list.js
@@ -3,6 +3,7 @@ class LinkedList{
         this.head = null;
     }
 
+    //adds a new node containing value to the end of the list
     append(value){
         if(this.head == null){
             this.head = new Node(value);
@@ -16,6 +17,7 @@ class LinkedList{
         }
     }
 
+    // adds a new node containing value to the start of the list
     prepend(value){
         let temp = null;
         if(this.head !== null){
@@ -25,6 +27,7 @@ class LinkedList{
         }
     }
 
+    // returns the total number of nodes in the list
     size(){
         let total=0;
         let temp = this.head;
@@ -35,11 +38,13 @@ class LinkedList{
         return total;
     }
 
+    // returns the first node in the list
     getHeadValue(){
         if(this.head === null) return null;
         return this.head.value;
     }
 
+    //  returns the last node in the list
     getTailValue(){
         let temp = this.head;
         while(temp.nextNode !== null){
@@ -48,6 +53,7 @@ class LinkedList{
         return temp.value; 
     }
 
+    //  returns the node at the given index
     findIndexValue(index){
         let tmpIndex = 0;
         let temp = this.head;
@@ -58,6 +64,7 @@ class LinkedList{
         return temp.value;
     }
 
+    // removes the last element from the list
     popElement(){
         if(!this.head) return 'Empty List'
         let curr = this.head;
@@ -69,6 +76,7 @@ class LinkedList{
         prev ? prev.nextNode = null : this.head = null;
     }
 
+    // returns true if the passed in value is in the list and otherwise returns false
     containsValue(value){
         let temp = this.head
         while(temp !== null){
@@ -78,6 +86,7 @@ class LinkedList{
         return false;
     }
 
+    // returns the index of the node containing value, or null if not found
     findIndex(value){
         let index = 0;
         let temp = this.head;
@@ -88,13 +97,43 @@ class LinkedList{
         }
     }
 
+    // prints the value of each node 
     listToString(){
         let temp = this.head;
-        while(temp.nextNode !== null){
-            console.log(`(${temp.value}) -->`)
+        let toString = ``;
+        while(temp !== null){
+            toString += `(${temp.value}) -->`
             temp = temp.nextNode;
         }
-        console.log(`${temp.nextNode}`);
+        toString += `(null)`
+        return toString
+    }
+
+    // inserts a new node with the provided value at the given index
+    insertAt(value, index){
+        let temp = this.head;
+        let prev,followingNode = null;
+        let tempIndex = 0;
+        while(temp !== null && tempIndex !== index){
+            prev = temp;
+            temp, followingNode= temp.nextNode;
+            tempIndex++
+        }
+        const newNode = new Node(value);
+        temp.nextNode = followingNode;
+        prev.nextNode = temp;   
+    }
+
+    //removes the node at the given index
+    removeAt(index){
+        let temp = this.head;
+        let prev,followingNode = null;
+        let counter = 0;
+        while(index !== counter){
+            prev = temp;
+            temp, followingNode = temp.nextNode; 
+        }
+        prev.Nextnode = followingNode;
     }
 }
 
@@ -119,4 +158,10 @@ console.log('Get Tail Value', lnkList.getTailValue()); //4
 console.log('Find Index Value', lnkList.findIndexValue(2)) //3
 console.log('Contains Value', lnkList.containsValue(2)); //true
 console.log('Find Index of Value', lnkList.findIndex(1)); //0
-console.log('Conver to String', lnkList.listToString())// (1) --> (2) --> (3) --> (4) --> null
+console.log('Convert to String', lnkList.listToString()); // (1) --> (2) --> (3) --> (4) --> null
+lnkList.popElement();
+console.log('Display last Element Removed', lnkList.listToString()); // (1) --> (2) --> (3) --> null
+// lnkList.insertAt(5,2); 
+// console.log('Pring list', lnkList.listToString());// (1) --> (2) --> (5) --> (3) --> null
+// console.log('Remove at', lnkList.removeAt(1));
+// console.log('Pring list', listToString());
